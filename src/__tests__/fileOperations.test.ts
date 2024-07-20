ce7e9e9a64ee9e10127f9fbbb8267362e2dcabe2
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-07-20 21:07:29 +0200

    Aggiunto test per metodo che verifica esistenza file

--- src/__tests__/fileOperations.test.ts
+++ src/__tests__/fileOperations.test.ts
@@ -1,3 +1,29 @@
-import * as fs from 'fs';// gestione dei file
-import { readFile } from '../index'; //importo i metodi da testare in index
+import * as fs from 'fs';
+import { fileExists } from '../index'; // importa il metodo da testare
 
+describe('fileExists', () => {
+  const testFilePath = './temp_test_file.txt';
+
+  beforeEach(() => {
+    // Creare un file temporaneo per il test
+    fs.writeFileSync(testFilePath, 'Temporary file content');
+  });
+
+  afterEach(() => {
+    // Rimuove il file temporaneo dopo i test
+    if (fs.existsSync(testFilePath)) {
+      fs.unlinkSync(testFilePath);
+    }
+  });
+
+  it('Ritorna true se il file temporaneo esiste', () => {
+    const result = fileExists(testFilePath);
+    expect(result).toBe(true); //Se torna true per un file temporaneo ritornerà true per tutti
+  });
+
+  it('Ritorna false se il file temporaneo non esiste', () => {
+    const nonExistentFilePath = './non_existent_file.txt';
+    const result = fileExists(nonExistentFilePath);
+    expect(result).toBe(false);
+  });
+});
